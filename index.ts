a293438bdbb6d2535775d53e68828f756b49517d
Rizky Firmansyah <rizky.firmansyah@solutif.co.id>
2025-05-27 14:02:54 +0700

    first commit biome

---
+++ index.ts
@@ -0,0 +1,13 @@
+const local: any = {
+    'zh-CN': {
+        'app.name': '应用名称',
+        'app.description': '应用描述',
+        'app.version': '版本',
+        'app.author': '作者',
+        'app.license': '许可证',
+        'app.homepage': '主页',
+        'app.repository': '代码仓库',
+        'app.bugs': '问题反馈',
+        'app.documentation': '文档',
+    },
+}
